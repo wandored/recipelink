b86b13d175d178a7b8dc3ffec6bf13c905e22317
wandored <chefk67@gmail.com>
2024-01-11 15:04:06 -0500

    updated output

--- wine_linkCheck.py
+++ wine_linkCheck.py
@@ -19,6 +19,7 @@ def main():
 
     os.system('clear')
 
+    merge1 = pd.DataFrame()
     for store in store_list:
         menuitems = pd.read_csv('./linkcheck/wine_menu_items.csv', sep=',', usecols=['Name', 'Recipe'])
         ingredients = pd.read_csv('./linkcheck/wine_ingredients.csv', sep=',', usecols=['Item', 'Recipe'])
@@ -29,22 +30,40 @@ def main():
 
         menuitems['Name'] = menuitems['Name'].str.replace('Steakhouse - ', '')
         menuitems.rename(columns={'Name': 'Toast'}, inplace=True)
-
         ingredients.rename(columns={'Item': 'Purchase Item'}, inplace=True)
         stockCount.rename(columns={'Item': 'Purchase Item'}, inplace=True)
         vendorItems.rename(columns={'Item': 'Purchase Item'}, inplace=True)
 
         merge1 = pd.merge(menuitems, ingredients, on='Recipe', how='left')
-        merge1.drop(columns=['Recipe'], inplace=True)
-        merge2 = pd.merge(merge1, stockCount, on='Purchase Item', how='outer')
+        #merge1.drop(columns=['Recipe'], inplace=True)
+        in_toast = pd.merge(merge1, stockCount, on='Purchase Item', how='left')
+        in_stockcount = pd.merge(merge1, stockCount, on='Purchase Item', how='right')
+        in_both = pd.merge(merge1, stockCount, on='Purchase Item', how='inner')
 
-        linkCheck = pd.merge(merge2, vendorItems, on='Purchase Item', how='left')
+        in_toast.sort_values(by='Purchase Item', inplace=True)
+        in_toast.drop(columns=['Recipe'], inplace=True)
+        in_toast = in_toast[in_toast['Purchase Item'].isna()]
+
+        in_stockcount.sort_values(by='Purchase Item', inplace=True)
+        in_stockcount.drop(columns=['Recipe'], inplace=True)
+        in_stockcount = in_stockcount[in_stockcount['Toast'].isna()]
+
+        in_both.sort_values(by='Purchase Item', inplace=True)
+        in_both.drop(columns=['Recipe'], inplace=True)
+
+
+        #linkCheck = pd.merge(merge2, vendorItems, on='Purchase Item', how='left')
 #        linkCheck.dropna(subset=['Toast'], thresh=1, inplace=True)
+        ic(merge1)
 
         with pd.ExcelWriter(f'./output/{store}_WineCheck.xlsx') as writer:     # pylint: disable=abstract-class-instantiated
-            linkCheck.sort_values(by='Purchase Item', inplace=True)
-            linkCheck.to_excel(writer)
-        print(linkCheck.info())
+            in_toast.to_excel(writer, sheet_name='In Toast')
+            in_stockcount.to_excel(writer, sheet_name='In StockCount')
+            in_both.to_excel(writer, sheet_name='In Both')
+
+    with pd.ExcelWriter('./output/WineCheck.xlsx') as writer:     # pylint: disable=abstract-class-instantiated
+        merge1.sort_values(by='Toast', inplace=True)
+        merge1.to_excel(writer)
 
 
 if __name__ == '__main__':
